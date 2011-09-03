1581faeb5b5d9dd92c0f354622b4ba463fb3fb61
Christopher Rigor <crigor@gmail.com>
2011-09-03 09:53:44 +0800

    new rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetup::Application.config.secret_token = 'a0dc5df45f12c5203ab9c29a9e586744fcdf41784ef8c548c32f54f938728b012e0d9d00b0641a4f46668ee729ffb3cc74ef7b539b96275a76481fcb82e37514'
